68ac57dc1584893f9ee11610a3d856c2a14a1a26
“karandepale” <karandepale111@gmail.com>
2023-06-02 09:56:29 +0530

    Add: Creating simple linked list for 56,30,70

--- LinkedListDay14/Program.cs
+++ LinkedListDay14/Program.cs
@@ -1,16 +1,63 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LinkedListDay14
 {
+    
+
+    public class LinkedList
+    {
+        public Node Head { get; set; }
+
+        public LinkedList()
+        {
+            Head = null;
+        }
+
+        public void AddNode(int data)
+        {
+            Node newNode = new Node(data);
+
+            if (Head == null)
+            {
+                Head = newNode;
+            }
+            else
+            {
+                Node current = Head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+            }
+        }
+
+        public void Display()
+        {
+            Node current = Head;
+
+            while (current != null)
+            {
+                Console.Write(current.Data + " ");
+                current = current.Next;
+            }
+            Console.WriteLine();
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Linked list program ....");
+
+            LinkedList linkedList = new LinkedList();
+
+            linkedList.AddNode(56);
+            linkedList.AddNode(30);
+            linkedList.AddNode(70);
+
+            linkedList.Display();
         }
     }
 }
